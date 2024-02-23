6d038dced7fc04353f2a558c8b1e65320b12c0d0
gleb <belkov.glebka@yandex.ru>
2024-02-23 21:10:59 +0300

    add models

---
+++ questionnaire/Models/Answer.swift
@@ -0,0 +1,8 @@
+//
+//  Answer.swift
+//  questionnaire
+//
+//  Created by Глеб on 23.02.2024.
+//
+
+import Foundation
---
+++ questionnaire/Models/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  questionnaire
+//
+//  Created by Глеб on 23.02.2024.
+//
+
+import Foundation
