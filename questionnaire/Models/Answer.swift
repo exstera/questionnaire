3127947ce167ef4136c15a8ef67bd69621d3e33a
gleb <belkov.glebka@yandex.ru>
2024-02-24 11:31:50 +0300

    add  models

--- questionnaire/Models/Answer.swift
+++ questionnaire/Models/Answer.swift
@@ -5,4 +5,28 @@
 //  Created by Глеб on 23.02.2024.
 //
 
-import Foundation
+
+struct Answer {
+    let text: String
+    let type: AnimalType
+}
+
+enum AnimalType: Character {
+    case dog = "🐶"
+    case cat = "😺"
+    case rabbit = "🐰"
+    case turtle = "🐢"
+    
+    var definition: String {
+        switch self {
+        case .dog:
+            return "Собака"
+        case .cat:
+            return "Кошка"
+        case .rabbit:
+            return "Кролик"
+        case .turtle:
+            return "Черепаха"
+        }
+    }
+}
--- questionnaire/Models/Question.swift
+++ questionnaire/Models/Question.swift
@@ -5,4 +5,35 @@
 //  Created by Глеб on 23.02.2024.
 //
 
-import Foundation
+enum ResponseType {
+    case single
+    case multiple
+    case range
+}
+
+
+struct Question {
+    let text: String
+    let type: ResponseType
+    let answers: [Answer]
+}
+
+
+extension Question {
+    static func getQuestion() -> [Question] {
+        return [
+            Question(text: "Какую пищу вы предпочитаете?", type: .single, answers: [Answer(text: "Стейк", type: .dog),
+                                                                                    Answer(text: "Рыба", type: .cat),
+                                                                                    Answer(text: "Морковка", type: .rabbit),
+                                                                                    Answer(text: "Кукуруза", type: .turtle)]),
+            Question(text: "Какую пищу вы предпочитаете?", type: .multiple, answers: [Answer(text: "Плавать", type: .dog),
+                                                                                    Answer(text: "Спать", type: .cat),
+                                                                                    Answer(text: "Обниматься", type: .rabbit),
+                                                                                    Answer(text: "Есть", type: .turtle)]),
+            Question(text: "Какую пищу вы предпочитаете?", type: .range, answers: [Answer(text: "Обожаю", type: .dog),
+                                                                                    Answer(text: "Обожаю", type: .cat),
+                                                                                    Answer(text: "Ненавижу", type: .rabbit),
+                                                                                    Answer(text: "Ненавижу", type: .turtle)]),
+            ]
+    }
+}
